0bc032268d2acd65dabf0a7b5def8749a6bda2a0
Alexz <alexislc330@gmail.com>
2024-07-14 16:54:53 -0600

    [ADD] se agrego las publicaciones del usuario en su perfil

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.app')
 
 @section('titulo')
-    Perfil: {{$user->username}}
+    Perfil: {{ $user->username }}
 @endsection
 
 @section('contenido')
     <div class="flex justify-center">
         <div class="w-full md:w-8/12 lg:w-6/12 flex flex-col items-center md:flex-row">
             <div class="w-8/12 lg:w-6/12 px-5">
-                <img src="{{asset('img/usuario.svg')}}" alt="Usuario" >
+                <img src="{{ asset('img/usuario.svg') }}" alt="Usuario">
             </div>
             <div class="md:w-8/12 lg:w-6/12 px-5 flex flex-col items-center md:justify-center md:items-start py-10 md:10">
-                <p class="text-gray-700 text-2xl">{{$user->username}}</p>
+                <p class="text-gray-700 text-2xl">{{ $user->username }}</p>
                 <p class="text-gray-800 text-sm mb-3 font-bold mt-5">
                     0
                     <span class="font-normal"> Seguidores</span>
@@ -30,4 +30,25 @@
             </div>
         </div>
     </div>
+
+    <section class="container mx-auto mt-10">
+        <h2 class="text-4xl text-center font-black my-10">Publicaciones</h2>
+        @if ($posts->count())
+            <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+                @foreach ($posts as $post)
+                    <a href="">
+                        <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen del post {{ $post->titulo }}">
+                    </a>
+                @endforeach
+            </div>
+
+            <div class="my-10">
+                {{ $posts->links('pagination::tailwind') }}
+            </div>
+        
+        @else
+            <p class="text-gray-600 uppercase text-sm text-center font-bold">No hay posts</p>
+        @endif
+
+    </section>
 @endsection
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 export default {
   content: ['./resources/**/*.blade.php',
     './resources/**/*.blade.js',
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
 
   ],
   theme: {
